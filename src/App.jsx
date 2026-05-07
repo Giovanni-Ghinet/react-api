2828abe0144d1d03286a96b06b2964c07ee0c146
Giovanni Ghinet <ghinetgiovanni88@gmai.com>
2026-05-07 17:23:47 +0200

    corretta la card dato il flex e sistemate le immagini, lasciato bianco perchè blu scuro non mi piaceva

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const fetchActresses = () => {
       return response.json();
     })
     .then(data => {
-      
+      console.log(data)
       return data;
     });
 };
@@ -31,37 +31,40 @@ function App() {
 
   return (
     <div className="container">
-      <div className="row">
-        {actressList.map(actress => (
-          <div className="col-md-4 mb-4" key={actress.id || actress.name}>
-            <div
-              className="card h-100"
-              style={{ cursor: 'pointer' }}
-              onClick={() => setSelectedActress(actress)}
-            >
-              <img
-                src={actress.image}
-                className="card-img-top"
-                alt={actress.name}
-                style={{ height: '200px', objectFit: 'cover' }}
-              />
-              <div className="card-body d-flex flex-column">
-                <h5 className="card-title">{actress.name}</h5>
-                <h6 className="card-subtitle mb-2 text-body-secondary">
-                  {actress.nationality}, nata nel {actress.birth_year}
-                </h6>
-                <p className="card-text flex-grow-1">
-                  <small>{actress.bio}</small>
-                </p>
-                <p className="card-text">
-                  <strong>Riconoscimenti:</strong> {actress.awards}
-                </p>
-              </div>
+  <div className="row">
+    {actressList.map(actress => (
+      <div className="col-md-4 mb-4" key={actress.id || actress.name}>
+        <div className="card d-flex flex-column align-items-center text-center">
+          <h5 className="card-title mt-2">{actress.name}</h5>
+          <div className="container">
+            <img
+              src={actress.image}
+              className="card-img-top"
+              alt={actress.name}
+              style={{ height: '400px', whidth: '200px', objectFit: 'contain', width: '100%' }}
+            />
+          </div>
+          <div className="card-body d-flex flex-column align-items-center">
+            <h6 className="card-subtitle mb-2 text-body-secondary">
+                {actress.birth_year} {actress.nationality}
+            </h6>
+            <div className="card-text flex-grow-1">
+              <p>{actress.biography}</p>
+            </div>
+            <div className="card-text text-danger">
+              <strong>Known For:</strong>
+              <p>{actress.known_for}</p> 
+            </div>
+            <div className="card-text text-warning">
+              <strong>Awards:</strong>
+              <p>{actress.awards}</p> 
             </div>
           </div>
-        ))}
+        </div>
       </div>
-    </div>
+    ))}
+  </div>
+</div>
   );
 }
 export default App;
